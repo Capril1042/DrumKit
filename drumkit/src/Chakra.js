478c0516dee22f1f24c439a7c007afc43a966708
Capril1042 <Caprilewis1042@gmail.com>
2019-07-17 22:15:36 -0400

    adds onclck function for chakras

--- drumkit/src/Chakra.js
+++ drumkit/src/Chakra.js
@@ -4,21 +4,23 @@ import './Chakra.css';
 import ChakraData from'./ChakraData';
 import ChakraInfo from './ChakraInfo';
 
+
 function Chakra() {
   const [data] = useState({chakras: ChakraData.reverse()});
-  
- 
+  const [chakra, setChakra] = useState(null);
 
   return   ( 
-  <div>
+    <div>
   {data.chakras.map(item => (
     <div key={item.id} 
     style={{backgroundColor: `${item.color}`}} 
     className="ChakraOrb"
+    onClick={()=> setChakra(ChakraData[ChakraData.reverse().findIndex(obj=>obj.id === item.id)])}
     >
       {item.note}
     </div>
   ))}
+    
 </div>
 );
 }
