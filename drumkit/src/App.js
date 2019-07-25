1a351087572f86858c3f8519bd784580b1ccb634
Capril1042 <Caprilewis1042@gmail.com>
2019-07-25 16:33:20 -0400

    reorganizes code

--- drumkit/src/App.js
+++ drumkit/src/App.js
@@ -1,16 +1,45 @@
-import React from "react";
-import Chakra from "./Chakra";
+import React, { useState } from "react";
+
+import ChakraData from "./ChakraData";
 import ChakraInfo from "./ChakraInfo";
+
+import "./Chakra.css";
 import "./App.css";
 
 function App() {
+  const [data] = useState({ chakras: ChakraData.reverse() });
+  const [chakra, setChakra] = useState(null);
+
+  const handleClick = e => {
+    let chakraId = e.target.getAttribute("value");
+    console.log(typeof Number(chakraId));
+    let newChakra = data.chakras.reverse()[Number(chakraId) - 1];
+    setChakra({ chakra: newChakra });
+    e.preventDefault();
+
+    console.log(chakra);
+  };
+
   return (
     <div className="App">
       <div className="Chakra-Container">
-        <Chakra />
+        <div>
+          {data.chakras.map(item => (
+            <div
+              key={item.id}
+              style={{ backgroundColor: `${item.color}` }}
+              className="ChakraOrb"
+              value={item.id}
+              name={item.nameEnglish}
+              onClick={handleClick}
+            >
+              {item.note}
+            </div>
+          ))}
+        </div>
       </div>
       <div className="Information-Container">
-        <ChakraInfo />
+        <ChakraInfo data={chakra} />
       </div>
     </div>
   );
--- drumkit/src/ChakraInfo.js
+++ drumkit/src/ChakraInfo.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './ChakraInfo.css';
+import React, { useState, useEffect } from "react";
+import "./ChakraInfo.css";
+import "./Chakra.css";
 
-
-function ChakraInfo() {
+function ChakraInfo(props) {
+  // const [chakra]= useState(props.chakra);
+  // useEffect(()=> console.log(chakra));
+  const information =
+    props.data === null
+      ? console.log("undefined")
+      : console.log(props.data.chakra.nameEnglish);
   return (
     <div className="ChakraInfo">
-    Information
+      Information
+      {information}
     </div>
   );
 }
